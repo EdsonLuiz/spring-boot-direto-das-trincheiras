954f49389396de624fe6eab76b347aedc8c50889
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2026-01-07 18:48:01 -0300

    refactor: update method names

--- anime-service/src/main/java/com/edson/mapper/ProducerMapper.java
+++ anime-service/src/main/java/com/edson/mapper/ProducerMapper.java
@@ -2,8 +2,10 @@ package com.edson.mapper;
 
 import com.edson.domain.Producer;
 import com.edson.request.ProducerPostRequest;
+import com.edson.request.ProducerPutRequest;
 import com.edson.response.ProducerGetResponse;
 import com.edson.response.ProducerPostResponse;
+import com.edson.response.ProducerPutResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
@@ -16,11 +18,15 @@ public interface ProducerMapper {
 
     @Mapping(target = "createdAt", expression = "java(java.time.LocalDateTime.now())")
     @Mapping(target = "id", expression = "java(java.util.concurrent.ThreadLocalRandom.current().nextLong(0, 100_000))")
-    Producer toEntity(ProducerPostRequest request);
+    Producer fromProducerPostRequestToEntity(ProducerPostRequest request);
 
     ProducerPostResponse toPostResponse(Producer producer);
 
     ProducerGetResponse toGetResponse(Producer producer);
 
     List<ProducerGetResponse> toGetResponse(List<Producer> producer);
+
+    Producer fromProducerPutRequestToEntity(ProducerPutRequest request);
+
+    ProducerPutResponse toPutResponse(Producer producer);
 }

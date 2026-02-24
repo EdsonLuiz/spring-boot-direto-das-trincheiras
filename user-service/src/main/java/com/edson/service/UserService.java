bc62b6c717acf3d367938a14ce32d9fa4c332839
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2026-02-24 15:56:18 -0300

    feat: add assertions

--- user-service/src/main/java/com/edson/service/UserService.java
+++ user-service/src/main/java/com/edson/service/UserService.java
@@ -18,35 +18,48 @@ public class UserService {
     private final UserRepository repository;
 
     public List<User> findAll(String firstName) {
-        if(firstName == null) {
+        if (firstName == null) {
             return repository.findAll();
         }
         return repository.findByFirstNameIgnoreCase(firstName);
     }
 
     public User findById(Long id) {
-        return repository.findById(id)
-                .orElseThrow(() -> new NotFoundException("User not found"));
+        return repository.findById(id).orElseThrow(() -> new NotFoundException("User not found"));
     }
 
     @Transactional
     public User create(User user) {
+        assertEmailDoesNotExist(user.getEmail());
         return repository.save(user);
     }
 
     @Transactional
     public void deleteById(Long id) {
-        asserUserExists(id);
+        this.asserUserExists(id);
         repository.deleteById(id);
     }
 
     @Transactional
     public void update(User user) {
         this.findById(user.getId());
+        this.assertEmailDoesNotExist(user.getEmail(), user.getId());
         repository.save(user);
     }
 
-    public void asserUserExists(Long id){
+    public void asserUserExists(Long id) {
         this.findById(id);
+    }
+
+    public void assertEmailDoesNotExist(String email) {
+        repository.findByEmailIgnoreCase(email).ifPresent(UserService::throwEmailExistsException);
+    }
+
+    public void assertEmailDoesNotExist(String email, Long id) {
+        repository.findByEmailIgnoreCase(email).ifPresent(UserService::throwEmailExistsException);
+    }
+
+    private static void throwEmailExistsException(User user) {
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Email %s already exists".formatted(user.getEmail()));
     }
 }

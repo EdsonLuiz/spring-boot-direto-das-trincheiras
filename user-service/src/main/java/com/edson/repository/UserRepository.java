ff19e07d596c48e6681b33c709d3619288380b45
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2026-02-24 10:24:09 -0300

    feat: add query to find by email

--- user-service/src/main/java/com/edson/repository/UserRepository.java
+++ user-service/src/main/java/com/edson/repository/UserRepository.java
@@ -4,7 +4,9 @@ import com.edson.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findByFirstNameIgnoreCase(String firstName);
+    Optional<User> findByEmailIgnoreCase(String email);
 }
